d1753c052c507cdaddbacdaf063b7f596444cd34
Steve Wang <iswangheng@gmail.com>
2012-05-31 00:57:02 +0800

    okay, lets commit first..just in case

--- static/js/event.js
+++ static/js/event.js
@@ -2,6 +2,7 @@
 jQuery(function(){  
     change_active_nav();
     show_keywords_freqs();
+    get_keywords_array();
 });
 
 //event.html:	chang the nav-collapse active state
@@ -54,3 +55,70 @@ jQuery('.keyword').click(function(){
     var current_keyword = jQuery(this).text()
     jQuery('#current_keyword').text(current_keyword).hide().fadeIn(400);
 });
+
+
+//event.html:   get 5 keywords arrays and then invoke display_all_keywords_graph to display them!!
+function get_keywords_array(){
+  var line1=[['23-May-08', 578.55], ['20-Jun-08', 566.5], ['25-Jul-08', 480.88], ['22-Aug-08', 509.84], 
+      ['26-Sep-08', 454.13], ['24-Oct-08', 379.75], ['21-Nov-08', 303], ['26-Dec-08', 308.56], 
+      ['23-Jan-09', 299.14], ['20-Feb-09', 346.51], ['20-Mar-09', 325.99], ['24-Apr-09', 386.15]];
+  var line2=[['23-May-08', 518.55], ['20-Jun-08', 516.5], ['25-Jul-08', 280.88], ['22-Aug-08', 209.84], 
+      ['26-Sep-08', 464.13], ['24-Oct-08', 389.75], ['21-Nov-08', 103], ['26-Dec-08', 238.56], 
+      ['23-Jan-09', 199.14], ['20-Feb-09', 316.51], ['20-Mar-09', 355.99], ['24-Apr-09', 186.15]];
+  var line3=[['23-May-08', 418.55], ['20-Jun-08', 416.5], ['25-Jul-08', 280.88], ['22-Aug-08', 209.84], 
+      ['26-Sep-08', 464.13], ['24-Oct-08', 389.75], ['21-Nov-08', 103], ['26-Dec-08', 238.56], 
+      ['23-Jan-09', 199.14], ['20-Feb-09', 316.51], ['20-Mar-09', 355.99], ['24-Apr-09', 186.15]];
+  var line4=[['23-May-08', 318.55], ['20-Jun-08', 316.5], ['25-Jul-08', 280.88], ['22-Aug-08', 209.84], 
+      ['26-Sep-08', 464.13], ['24-Oct-08', 389.75], ['21-Nov-08', 103], ['26-Dec-08', 238.56], 
+      ['23-Jan-09', 199.14], ['20-Feb-09', 316.51], ['20-Mar-09', 355.99], ['24-Apr-09', 186.15]];
+  var line5=[['23-May-08', 218.55], ['20-Jun-08', 216.5], ['25-Jul-08', 280.88], ['22-Aug-08', 209.84], 
+      ['26-Sep-08', 464.13], ['24-Oct-08', 389.75], ['21-Nov-08', 103], ['26-Dec-08', 238.56], 
+      ['23-Jan-09', 199.14], ['20-Feb-09', 316.51], ['20-Mar-09', 355.99], ['24-Apr-09', 186.15]];
+  display_all_keywords_graph(line1, line2, line3, line4, line5);
+}
+
+
+//event.html:   #chartdiv display all 5 keywords graph
+//TODO
+function display_all_keywords_graph(line1, line2, line3, line4, line5){
+  var plot1 = $.jqplot('chartdiv', [line1,line2,line3,line4,line5], {
+      title:'Keywords Frequency Graph', 
+ 	  series:[ 
+          {   
+ 			color: '#0101DF', 
+          },  
+          {   
+ 			color: '#FF8000',
+          },  
+          {   
+ 			color: '#B404AE',
+          },  
+          {   
+ 			color: '#3ADF00', 
+          },  
+          {   
+ 			color: '#000000',
+          },  
+      ],  
+      axes:{
+        xaxis:{
+          renderer:$.jqplot.DateAxisRenderer,
+          tickOptions:{
+            formatString:'%b&nbsp;%#d'
+          } 
+        },
+        yaxis:{
+          tickOptions:{
+            formatString:'$%.2f'
+            }
+        }
+      },
+      highlighter: {
+        show: true,
+        sizeAdjust: 7.5
+      },
+      cursor: {
+        show: true
+      }
+  });  
+}
